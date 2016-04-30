1bd9320ceb5277a7c35edc594810c0c2e1f2e5c3
Jamie Hill <jhill@amelco.co.uk>
2016-04-30 17:09:40 +0100

    more consolidations

--- src/util/RouteUtil.js
+++ src/util/RouteUtil.js
@@ -1,18 +1,5 @@
 /**
  * @param to
- * @returns {{onEnter: onEnter}}
- */
-export const indexRedirect = (to) =>{
-    return {
-        path: '/',
-        onEnter: (next, replace) => {
-            replace(`/en/${to}`);
-        }
-    }
-};
-
-/**
- * @param to
  * @returns {{onEnter: (function())}}
  */
 export const indexRoute = (to) => {
